93e3456d5738c1fa59168c8eef3dcd80b729e2c7
Abrela <38386958+Abrela@users.noreply.github.com>
2019-06-06 19:37:25 +0200

    Add files via upload

--- multipleSequenceAlignment.m
+++ multipleSequenceAlignment.m
@@ -28,22 +28,66 @@ end
 maxScoreRow = max(scoreRow);
 [coordinateRowX, coordinateRowY] = find(maxScoreRow == scoreRow);
   
-seq = fastaStruct(coordinateRowX).sequence;
+amountSeq = length(fastaStruct); %ilosc sekwencji
+seq = fastaStruct(coordinateRowX).sequence; %centrum gwiazdy przypisane do zmiennej 
 
-%global alignment
-for j = 1:length(scoreRow)
+ alignment = cell(amountSeq-1); %cell w której beda zapisywane dopasowania, oprócz star
+ star = ''; %zmienna do zapisu centrum gwiazdy
+
+%multiple alignment
+for j = 1:(amountSeq)
     
     if isequal(seq, fastaStruct(j).sequence)
-      seq = fastaStruct(j).sequence;
+
     else
       [ mNW, matrixSize, bias, vertical, horizontal ]= ...
       matrixScore(seq, fastaStruct(j).sequence, match, mismatch, gap);
       [pathOut, identityOut, gapsOut, s1Out, s2Out, lengthAlignmentOut, scoreOut] = ...
       matchPath(matrixSize, bias, vertical, horizontal, seq, fastaStruct(j).sequence, match, mismatch, gap, mNW );
-      fastaStruct(j).sequence = s2Out;
-    end   
+      alignment{j,1} = s2Out;
+      star = s1Out;
 
-end 
+      if j > 2 %petla uruchamiana dopiero od trzeciej iteracji
+        m = length(alignment);
+        disp("M: " + m);
+      if m > 1 
+          seq1 = star;
+          disp("Seq1: " + seq1);
+          seq2 = alignment{m-1,1};
+          disp("Seq2: " + seq2);
+          seqTemp = '';
+          
+           if length(seq1) > length(seq2) %jeœli seq2 krótsze od centrum gwiazdy to wyrównujemy d³ugoœæ obu 
+                 while length(seq1) ~= length(seq2)
+                     if length(seq1) > length(seq2)
+                     seq2 = strcat(seq2, '-');
+                     end
+                 end
+           end
+           
+          for k = 1:length(seq1)
+              disp("K: " + k);
+                 if(seq1(k) == '-')
+                    seqTemp = insertBefore(seq2,seq2(k),'-');
+                    disp("SeqTemp: " + seqTemp);
+                 end
+          end
+          alignment{m-1,1} = seqTemp;
+          disp("Alignment{}: " + alignment{m-1,1});
+          m = m-1;
+      end
+      end       
+    end
+
+end
+
+for i = 1:length(alignment)
+    
+    fastaStruct(i).sequence = alignment{i,1};
+    
+end
+
+fastaStruct(coordinateRowX(1,1)).sequence = star;
 
 A = [];
 for i = 1:length(scoreRow)
